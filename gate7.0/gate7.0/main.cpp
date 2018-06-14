03f00ead16aabcf968a8a1809276dac45af26855
gate80 <gr.baz.java.pas@mail.ru>
2018-06-14 12:13:03 +0500

    gate local
    v1

--- gate7.0/gate7.0/main.cpp
+++ gate7.0/gate7.0/main.cpp
@@ -1,37 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <functional>
-#include <fstream>
-#include <time.h>
-
-// test
-#include<cstdlib>
-
-
+#include <iostream> // cout
+#include <string>   // string
+#include <vector>   // vector
+#include <fstream>	// fille >> <<
+#include <sstream>	// ostringstream
+#include <time.h>	// time
 using namespace std;
-int max(int a, int b)
+void run(string command)
 {
-	return ((a < b) ? b : a);
+	system(command.c_str());
 }
-struct v
+class LXC {
+private:
+	string os;
+	string container;
+public:
+	LXC(string container_, string os_)
+	{
+		container = container_;
+		os = os_;
+	}
+	void installPackage()
+	{
+		run("sudo apt-get install lxc");
+	}
+	void create_container(string dist, string relese, string arch)
+	{
+		run(" lxc-create -t download -n " + container + " -- --dist " + dist + " --release " + relese + " --arch " + arch);
+	}
+	void start_container()
+	{
+		run("sudo lxc-start " + container);
+	}
+	void ls_container() // list box
+	{
+		run("sudo lxc-ls");
+	}
+	void info_container() //Подробная информация о контейнер
+	{
+		run("sudo lxc-info" + container);
+	}
+	void stop_container() // остоновка контейнера
+	{
+		run("sudo lxc-stop " + container);
+	}
+	void copy_container(string copy_name) // Создание клона контейнера (test-container должен быть остановлен): fix bag
+	{
+		run("sudo lxc-copy" + container + " -N" + copy_name);
+	}
+	void del_container()
+	{
+		run("sudo lxc-destroy " + container);
+	}
+	void freeze_container()
+	{
+		run("sudo lxc-freeze " + container);
+	}
+	void unfreeze_container()
+	{
+		run("sudo lxc-unfreeze " + container);
+	}
+	void memory_limit(int M)
+	{
+		run("sudo lxc-cgroup -n " + container + " memory.limit_in_bytes " + to_string(M) + "M");
+	}
+};
+class v
 {
-	string str;
-	string answer;
-	size_t str_hash;
-	void set_answer(string t) { answer = t; }
-	void set_str(string t)
+private:
+	string str, answer;
+public:
+	v(string answer_, string str_)
 	{
-		str = t;
-	}
-	string getstr()
-	{
-		return str;
-	}
-	void strhash()
-	{
-		hash<string> hash_fn; size_t str_hash = hash_fn(str);
+		str = str_;
+		answer = answer_;
 	}
 
 	/// checker
@@ -43,32 +85,40 @@ struct v
 			if (c != ' ')vanswer.push_back(c);
 		for (char c : str)
 			if (c != ' ')vstr.push_back(c);
-
-		for (int i = 0; i<max(vstr.size(), vanswer.size()); i++)
+		if (vstr.size() != vanswer.size())
+			return false;
+		for (int i = 0; i < vstr.size(); i++)
 			if (vstr[i] != vanswer[i])
-			{
 				return false;
-			}
 		return true;
 	}
 	bool str1() // GATE | GATE
 	{
 		return ((str == answer) ? true : false);
 	}
-	bool test()
+	bool str1_() // GATE_ | GATE
 	{
-		return true;
+		return ((str == answer + " " || str == answer) ? true : false);
+	}
+	bool no() // no fix a+n 1 || 1!=1
+	{
+		return ((str != answer && str != answer + " ") ? true : false);
 	}
 };
 
-void del_endl(string &s) // string test\n -> test
+void readFile(string fileName, string &str)
 {
-	s =  s.substr(0, s.size() - 1);
+	std::ifstream in(fileName.c_str(), std::ios::binary);
+	ostringstream out;
+	str.clear();
+	out << in.rdbuf();
+	out.str().swap(str);
 }
 int main(int argc, char *argv[])
 {
-	v v;
-	
+	clock_t time;
+
+	time = clock();
 	ifstream property("0001/property.txt", ios::in); // настройки задачи
 	ofstream log("log.txt", ios::out); // log
 	log << argv[0];
@@ -77,53 +127,27 @@ int main(int argc, char *argv[])
 	string infile, OUTfille;
 	property >> SIZET >> infile >> OUTfille;
 	// time
-	time_t rawtime;
-	struct tm * timeinfo;
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-	log << asctime(timeinfo);
 
-	log << "{test№;input;trueanswer;answer;true/false}\n";
+	log << "\n{test;input;trueanswer;answer;true/false}\n";
+	for (int i = 1; i <= SIZET; i++)
+	{
+		string testout, testin, output;
+		fstream input("0/" + infile, ios::out);							// input
+		readFile(("0001/output/" + to_string(i) + ".txt"), testout);	// output ответа правильного
+		readFile(("0001/input/" + to_string(i) + ".txt"), testin);		// input для теста
+		readFile(("0/" + OUTfille), output);							// ответ
+		input << testin;
 
+		v(testout, output);
+		log << i << ";" << testin << ";" << testout << ";" << output << ";" << "\n";
+		/////////////// close filles
+		input.close();
+		///////////////
+	}
 
-		for (int i = 1; i <= SIZET; i++)
-		{
-			
-			ifstream TestOut(("0001/output/" + to_string(i) + ".txt"), ios::in); // output ответа правильного
-			ifstream TestIn(("0001/input/" + to_string(i) + ".txt"), ios::in); // input для теста
-			ifstream output("0/" + OUTfille, ios::in); // ответ
-			fstream input("0/" + infile, ios::out); // input
-			string tmp, sout = "", sin = "", soutput = "";
-			// scan
-			while (getline(TestOut, tmp))
-				sout += tmp + "\n";
-			while (getline(TestIn, tmp))
-				sin += tmp + "\n";
-			input << sin;
+	// test polygon
 
-			system("start C:\\Users\\Иван\\Desktop\\gate7.0\\gate7.0\\0\\t.exe");
-			// run
-			while (getline(output, tmp))
-				soutput += tmp + "\n";
-
-			// delete \n
-			del_endl(sin);
-			del_endl(sout);
-			del_endl(soutput);
-
-
-			log << i << ";" << sin << ";" << sout << ";" << soutput << ";test\n";
-
-			/////////////// close filles
-			TestOut.close();
-			TestIn.close();
-			output.close();
-			input.close();
-			///////////////
-		}
-	
-		//test run failed  failed lol 
-		printf("%s\n", argv[0]);
-		system("pause");
+	time = clock() - time;
+	log << (double)time / CLOCKS_PER_SEC;
 	return 0;
 }
